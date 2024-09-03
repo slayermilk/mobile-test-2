cf2efacb3adeb007e5f6f73d4a4903880c6f3a22
Mufazzal <74534631+slayermilk@users.noreply.github.com>
2024-09-03 12:52:59 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    // Close the navbar when a link is clicked
+    $('.navbar-nav a').on('click', function () {
+        // Collapse the navbar
+        $('.navbar-collapse').collapse('hide');
+    });
+});
